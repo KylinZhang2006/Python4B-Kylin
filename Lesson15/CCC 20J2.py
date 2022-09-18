01042ff1e111fee390119585bc4a3566b8b26606
Heather Wang <heatherwang39@gmail.com>
2022-09-18 16:18:38 -0400

    updated 20j2.

--- Lesson15/CCC 20J2.py
+++ Lesson15/CCC 20J2.py
@@ -1,12 +1,16 @@
 """https://dmoj.ca/problem/ccc20j2"""
 
-P = int(input())
-N = int(input())
-R = int(input())
+P = int(input())  # total number
+N = int(input())  # people affected on the Day 0
+R = int(input())  # the number of people can be infected by 1 person
 
-day = N
-while day < P:
-    day += (N * R * R)
+day = 0
+infected_previous_day = N
+infected = infected_previous_day
+while infected <= P:
+    day += 1
+    infected += infected_previous_day * R
+    infected_previous_day = infected_previous_day * R
 
 print(day)
 
